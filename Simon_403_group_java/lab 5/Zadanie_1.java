52a60452898155b1190d366d74378068286017a5
Suirife <99396087+Suirife@users.noreply.github.com>
2022-11-16 10:52:36 +0700

    123

--- Simon_403_group_java/lab 5/Zadanie_1.java
+++ Simon_403_group_java/lab 5/Zadanie_1.java
@@ -1,42 +1,22 @@
 /*Simon 16.11*/
 import java.util.Scanner;
-public class Main
+public class lab5
 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
 		if ((n<10) && (n>-1)) {
 			switch (n) {
-				case 0:
-					System.out.println("Ноль");
-					break;
-				case 1:
-					System.out.println("Один");
-					break;
-				case 2:
-					System.out.println("Два");
-					break;
-				case 3:
-					System.out.println("Три");
-					break;
-				case 4:
-					System.out.println("Четыре");
-					break;
-				case 5:
-					System.out.println("Пять");
-					break;
-				case 6:
-					System.out.println("Шесть");
-					break;
-				case 7:
-					System.out.println("Семь");
-					break;
-				case 8:
-					System.out.println("Восемь");
-					break;
-				case 9:
-					System.out.println("Девять");
-					break;
+				case 0 -> System.out.println("Ноль");
+				case 1 -> System.out.println("Один");
+				case 2 -> System.out.println("Два");
+				case 3 -> System.out.println("Три");
+				case 4 -> System.out.println("Четыре");
+				case 5 -> System.out.println("Пять");
+				case 6 -> System.out.println("Шесть");
+				case 7 -> System.out.println("Семь");
+				case 8 -> System.out.println("Восемь");
+				case 9 -> System.out.println("Девять");
 			}
 		}
 		else {
