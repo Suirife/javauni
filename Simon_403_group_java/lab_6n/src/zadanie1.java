670bde32a0c7dd4d48177acaf4683dce08644a63
Suirife <99396087+Suirife@users.noreply.github.com>
2022-11-30 09:17:16 +0700

    first task

---
+++ Simon_403_group_java/lab_6n/src/zadanie1.java
@@ -0,0 +1,17 @@
+public class zadanie1 {
+    public static void main(String[] args){
+        final int MILLISECONDS_PER_SECOND = 1000  ,SECONDS_PER_MINUTE = 60,MINUTES_PER_HOUR = 60 ,HOURS_PER_DAY = 24;
+        long totalMilliseconds,currentHour, currentMinute, currentSecond,totalSeconds,totalMinutes,totalHours;
+        totalMilliseconds = System.currentTimeMillis();
+        totalSeconds = totalMilliseconds / MILLISECONDS_PER_SECOND;
+        currentSecond = totalSeconds % SECONDS_PER_MINUTE;
+        totalMinutes = totalSeconds / SECONDS_PER_MINUTE;
+        currentMinute = totalMinutes % MINUTES_PER_HOUR;
+        totalHours = totalMinutes / MINUTES_PER_HOUR;
+        totalHours = totalHours +7;
+        currentHour = totalHours % HOURS_PER_DAY;
+
+        System.out.println("Текущее время = "+currentHour + ":"+currentMinute + ":"+currentSecond + "GMT");
+    }
+
+}
